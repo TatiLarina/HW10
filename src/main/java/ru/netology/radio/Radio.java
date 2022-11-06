b6c0323fbb45cbf4b840a77d42caaca3be8e39b8
Tatyana Larina <jerrymurr@ya.ru>
2022-11-06 15:35:03 +0300

    station

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -12,4 +12,31 @@ public class Radio {
         return currentVolumeLvl;
     }
 
+    public int pressNext() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        }
+        else {
+            currentStation ++;
+        }
+        return currentStation;
+    }
+
+    public int pressPrev() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        }
+        else {
+            currentStation --;
+        }
+        return currentStation;
+    }
+
+    public int setCurrentStation(int newCurrentStation) {
+        if ((newCurrentStation > 9) | (newCurrentStation < 0)) {
+            return currentStation;
+        }
+        return currentStation = newCurrentStation;
+    }
+
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -9,7 +9,7 @@ public class RadioTest {
     public void shouldGetStation() {
         Radio radio = new Radio();
 
-        radio.currentStation = 5;
+        radio.setCurrentStation(5);
         int expected = 5;
         int actual = radio.getCurrentStation();
 
@@ -23,6 +23,101 @@ public class RadioTest {
         radio.currentVolumeLvl = 5;
         int expected = 5;
         int actual = radio.getCurrentVolumeLvl();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void pressNext0_9() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(0);
+        int expected = 0;
+        int actual;
+        for (int i = 1; i < 9; i++) {
+            radio.pressNext();
+            expected++;
+            actual = radio.currentStation;
+
+            Assertions.assertEquals(expected, actual);
+
+        }
+    }
+
+    @Test
+    public void pressNextIf9() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(9);
+        radio.pressNext();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void pressPrev0_9() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(9);
+        int expected = 9;
+        int actual;
+        for (int i = 1; i < 9; i++) {
+            radio.pressPrev();
+            expected--;
+            actual = radio.currentStation;
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void pressPrevIf0() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(0);
+        radio.pressPrev();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStation5() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(5);
+
+        int expected = 5;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationLess0() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationMore9() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
     }
